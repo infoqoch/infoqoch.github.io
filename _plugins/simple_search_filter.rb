ebaf8b207d0f5dbdfabf6ad159ae7626ab19cfd8
bae <infoqoch@gmail.com>
2021-12-04 23:29:37 +0900

    수정

--- _plugins/simple_search_filter.rb
+++ _plugins/simple_search_filter.rb
@@ -1,18 +1,16 @@
 module Jekyll
   module CharFilter
-    def remove_chars(input)
-      input.gsub! '\\','&#92;'
-      input.gsub! /\t/, '    '
+    def filtersss(input)
+      input.gsub! '\\',''
+      input.gsub! /\t/, ''
+      input.gsub! '"', ''
+      input.gsub! '<', ''
+      input.gsub! '>', ''
+      input.gsub! '/', ''
+      input.gsub! /\n/, ''
     end
   end
 end
 
 Liquid::Template.register_filter(Jekyll::CharFilter)
 
-class String
-  def strip_control_and_extended_characters()
-    chars.each_with_object("") do |char, str|
-      str << char if char.ascii_only? and char.ord.between?(32,126)
-    end
-  end
-end
